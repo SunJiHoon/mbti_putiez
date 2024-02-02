5efdbe601330235ad3c2950e708b43a75d726a0b
EunHyoPark <parkeh0724@gmail.com>
2024-02-02 16:31:22 +0900

    프로젝트 세팅을 했습니다.

---
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/MbtiPutiezApplication.java
@@ -0,0 +1,13 @@
+package putiez.mbti_putiez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiPutiezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiPutiezApplication.class, args);
+	}
+
+}
---
+++ mbti_putiez/src/test/java/putiez/mbti_putiez/MbtiPutiezApplicationTests.java
@@ -0,0 +1,13 @@
+package putiez.mbti_putiez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiPutiezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
