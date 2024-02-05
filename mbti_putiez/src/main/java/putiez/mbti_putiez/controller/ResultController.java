5a92827535b260028ffc7364cf504fa5a1ba8a6a
SunJiHoon <cangogi1@naver.com>
2024-02-06 07:19:30 +0900

    result 리팩터링

--- mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ResultController.java
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ResultController.java
@@ -28,45 +28,6 @@ public class ResultController {
         return "/results/results.html";
     }
 
-    @GetMapping("/resultTest2")
-    public String yourHandler2() {
-        return "redirect:/resultTest"; // 결과 페이지로 이동
-    }
-
-    @GetMapping("/resultTest")
-    public String yourHandler() {
-        return "results/resultTest.html"; // 결과 페이지로 이동
-    }
-
-    // @GetMapping("/result")
-    private static String getString(String[] mbtiElements, String value, int start, String first, String second) {
-        int i;
-        int count1 = 0;
-        int count2 = 0;
-        for (i = start; i < start + 3; i++) {
-            if (mbtiElements[i].equals(first)) {
-                count1++;
-            }
-            else {
-                count2++;
-            }
-        }
-
-        if (count1 > count2) {
-            value += first;
-        }
-        else {
-            value += second;
-        }
-        return value;
-    }
-
-    //@GetMapping("/result")
-    public String showResultPage() {
-        // 결과 페이지 보여주기
-        return "/results/results.html";
-    }
-
 }
 
 
