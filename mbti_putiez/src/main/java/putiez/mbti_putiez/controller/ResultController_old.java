fa5324ebeeadb3090ee7b737f9a77799285b998f
SunJiHoon <cangogi1@naver.com>
2024-02-06 02:06:58 +0900

    윈도우 방식 허용

--- mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ResultController_old.java
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ResultController_old.java
@@ -7,16 +7,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
+import java.util.Map;
 
 @Controller
 @Slf4j
 
 public class ResultController_old {
     @PostMapping("/result-old")
-    public String resultController(@RequestBody MBTIDTO_old mbtiDTO, Model model) {
-        String[] mbtiElements = mbtiDTO.getMbtiElements();
+    public String resultController(@RequestParam Map<String, String> formData
+                                   //,@RequestBody MBTIDTO_old mbtiDTO
+                , Model model) {
+        String str = formData.get("mbtiElements");
+        log.info(str);
+
+        // 문자열에서 괄호 및 쌍따옴표를 제거하고 쉼표로 구분된 각 요소를 추출
+        String[] mbtiElements = str.substring(1, str.length() - 1).split(",");
+
+        // 배열에 각 요소 추가
+        for (int i = 0; i < mbtiElements.length; i++) {
+            mbtiElements[i] = mbtiElements[i].replaceAll("\"", ""); // 쌍따옴표 제거
+        }
+
+        //String[] mbtiElements = ["x", "x", "x","x", "x", "x","x", "x", "x","x", "x", "x"];//mbtiDTO.getMbtiElements();
+        //String[] mbtiElements = //new String[]{formData.get("mbtiElements")};//{"x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"};
+
         log.info(Arrays.toString(mbtiElements));
 
         //mbtiElements 안에는 ["x", "x", "x","x", "x", "x","x", "x", "x","x", "x", "x"] 가 있다.
@@ -32,7 +49,8 @@ public class ResultController_old {
 
         model.addAttribute("key", "../assets/" + value + ".png");
         //src="../assets/${key}.png"
-        return "redirect:/result-old";
+        return "/results/results.html";
+        //return "redirect:/result-old";
     }
 
     private static String getString(String[] mbtiElements, String value, int start, String first, String second) {
