c11de83b00d3f9b33086851dc6367158114f69cb
SunJiHoon <cangogi1@naver.com>
2024-02-09 17:27:17 +0900

    테스트 페이지에 카카오톡 공유 기능 추가-진행중

---
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/TestController.java
@@ -0,0 +1,24 @@
+package putiez.mbti_putiez.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class TestController {
+    //api.key.KAKAO_API_KEY
+    @Value("${api.key.KAKAO_API_KEY}")
+    private String kakaoapikey;
+
+    @GetMapping("/test")
+    public String showTest(Model model){
+        model.addAttribute("kakaoapikey", kakaoapikey);
+        //log.info(kakaoapikey);
+
+        return "test/test";
+    }
+
+}
