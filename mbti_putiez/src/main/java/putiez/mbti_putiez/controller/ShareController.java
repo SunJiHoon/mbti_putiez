47680d8c62f9ad965347b8c3a6d415619f3cadb0
EunHyoPark <parkeh0724@gmail.com>
2024-02-16 23:33:35 +0900

    key 검증 로직을 추가했습니다.

--- mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController.java
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import putiez.mbti_putiez.entity.ShareInfo;
 import putiez.mbti_putiez.repository.MariaJPA_ShareInfo;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Controller
@@ -17,17 +19,23 @@ public class ShareController {
     }
 
     @GetMapping("/sharing/share")
-    public String sharePage(@RequestParam("mbti") String mbti, @RequestParam("uuid") String uuid, Model model) {
+    public String sharePage(@RequestParam("mbti") String mbti, @RequestParam("key") String uuid, Model model) {
         boolean check1, check2; //각각 db 내에 해당 mbti에 해당하는 key가 있는지, 그 key가 24시간 내의 링크인지 체크하는 변수
+        ShareInfo shareInfo = mariaJPAShareInfo.findByMbtiAndUuid(mbti, uuid).get();
 
-        if () {
+        if (shareInfo.getUuid() != null) {
             check1 = true;
         } else {
             check1 = false;
         }
 
         LocalDateTime adjustedTime = LocalDateTime.now().plusHours(9);
-        if () {
+        LocalDateTime timestamp = shareInfo.getCreateTime().toLocalDateTime();
+
+        Duration duration = Duration.between(timestamp, adjustedTime);
+        long hoursDifference = Math.abs(duration.toHours()); // 시간 단위로 변환하여 절대값 취함
+
+        if (hoursDifference <= 24) {
             check2 = true;
         } else {
             check2 = false;
@@ -35,9 +43,11 @@ public class ShareController {
 
         if (check1 && check2) {
             model.addAttribute("name", mbti);
+            model.addAttribute("value", mbti);
+            model.addAttribute("key", "/assets/" + mbti + ".png");
             return "/sharing/share";
         } else {
-            return "";
+            return "expired";
         }
 
     }
