bd244b832bfe21631941dd353aeaab4ba4ba39ca
EunHyoPark <parkeh0724@gmail.com>
2024-02-16 21:16:40 +0900

    share.html과 ShareController를 만들었습니다.

---
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController.java
@@ -0,0 +1,11 @@
+package putiez.mbti_putiez.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class ShareController {
+
+    @GetMapping("/sharing")
+    public String shareController() {
+        return "";
+    }
+}
