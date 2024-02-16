e8231f3b3e207f85f9f4ea4915102ecb993843d1
SunJiHoon <cangogi1@naver.com>
2024-02-16 23:16:34 +0900

    mbti와 uuid로 객체 찾는 로직 제작

--- mbti_putiez/src/main/java/putiez/mbti_putiez/repository/MariaJPA_ShareInfo.java
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/repository/MariaJPA_ShareInfo.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import putiez.mbti_putiez.entity.ShareInfo;
 import putiez.mbti_putiez.entity.puangMBTI;
 
+import java.util.Optional;
+
 public interface MariaJPA_ShareInfo extends JpaRepository<ShareInfo, Long> {
+    Optional<ShareInfo> findByMbtiAndUuid(String mbti, String uuid);
 
 }
