2596da179ad9eeee10d52b2e26ab9c5cdced361d
EunHyoPark <parkeh0724@gmail.com>
2024-02-16 21:59:40 +0900

    ShareController_api를 수정했습니다.

--- mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController.java
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController.java
@@ -2,6 +2,7 @@ package putiez.mbti_putiez.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -9,10 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 public class ShareController {
 
-
     @GetMapping("/sharing")
-    public String sharePage() {
-        //
+    public String sharePage(Model model) {
+        
         return "";
     }
 }
--- mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController_api.java
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController_api.java
@@ -1,21 +1,44 @@
 package putiez.mbti_putiez.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @Slf4j
 @RequestMapping("/api")
 public class ShareController_api {
 
-
     @PostMapping("/sharing")//요청경로 /api/sharing
-    public String sharePage() {
+    public Result sharePage() {
+        Result result = new Result();
         //json객체를 반환해주세요
         //생성된 난수와 mbti정보를 반환해주세요
-        return "";
+
+        UUID uuid = UUID.randomUUID();
+
+        // UUID를 문자열로 변환
+        String randomUUIDString = uuid.toString();
+
+        // 문자열에서 '-' 제거
+        String randomNumberString = randomUUIDString.replaceAll("-", "");
+
+        // 문자열의 일부를 추출하여 숫자로 변환
+        String randomNumberSubstring = randomNumberString.substring(0, 6);
+        int randomNumber = Integer.parseInt(randomNumberSubstring);
+
+        result.setRandom(randomNumber);
+        result.setMbti("ENFJ");
+        return result;
+    }
+
+    @Getter @Setter
+    static class Result {
+        private int random;
+        private String mbti;
+
     }
 }
