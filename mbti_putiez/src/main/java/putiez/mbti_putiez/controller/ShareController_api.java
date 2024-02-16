cdbfcf54ece43e3d0f70161e4ac2972d8b80b742
SunJiHoon <cangogi1@naver.com>
2024-02-16 21:22:53 +0900

    명세 작성

---
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController_api.java
@@ -0,0 +1,14 @@
+package putiez.mbti_putiez.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class ShareController {
+
+
+    @GetMapping("/sharing")
+    public String sharePage() {
+        //
+        return "";
+    }
+}
