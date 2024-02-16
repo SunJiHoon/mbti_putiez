a5b316dad251bb0920f12e4ea760ebc0f51641d7
EunHyoPark <parkeh0724@gmail.com>
2024-02-16 23:12:16 +0900

    sharePage에 url반환 로직을 추가했습니다.(진행중)

--- mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController.java
+++ mbti_putiez/src/main/java/putiez/mbti_putiez/controller/ShareController.java
@@ -3,14 +3,42 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import putiez.mbti_putiez.repository.MariaJPA_ShareInfo;
+
+import java.time.LocalDateTime;
 
 @Controller
 @Slf4j
 public class ShareController {
+    MariaJPA_ShareInfo mariaJPAShareInfo;
+    public ShareController(MariaJPA_ShareInfo mariaJPAShareInfo) {
+        this.mariaJPAShareInfo = mariaJPAShareInfo;
+    }
 
-    @GetMapping("/sharing")
-    public String sharePage(Model model) {
+    @GetMapping("/sharing/share")
+    public String sharePage(@RequestParam("mbti") String mbti, @RequestParam("uuid") String uuid, Model model) {
+        boolean check1, check2; //각각 db 내에 해당 mbti에 해당하는 key가 있는지, 그 key가 24시간 내의 링크인지 체크하는 변수
 
-        return "";
+        if () {
+            check1 = true;
+        } else {
+            check1 = false;
+        }
+
+        LocalDateTime adjustedTime = LocalDateTime.now().plusHours(9);
+        if () {
+            check2 = true;
+        } else {
+            check2 = false;
+        }
+
+        if (check1 && check2) {
+            model.addAttribute("name", mbti);
+            return "/sharing/share";
+        } else {
+            return "";
+        }
+
     }
 }
